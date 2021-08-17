e45e2c789a1b6cda99b63ab518c63f2d8c53376f
intarasorn1427 <genix1427@gmail.com>
2021-08-17 18:23:12 +0700

    first commit

---
+++ Mywork.js
@@ -0,0 +1,14 @@
+console.log("Hi")
+
+setTimeout(nameme,2000);
+
+function nameme(){
+  console.log("Chayada")
+}
+setTimeout(nameme2,4000);
+
+function nameme2(){
+  console.log("Intarasorn")
+}
+
+console.log("My name is");
